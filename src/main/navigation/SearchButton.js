0eaa5fa843cc788e6e4593e98efc183e30614510
pritam-patil <pritam18121990@gmail.com>
2019-03-31 18:20:31 +0530

    Added header, nav, main, content roles

--- src/main/navigation/SearchButton.js
+++ src/main/navigation/SearchButton.js
@@ -3,7 +3,7 @@ import "./SearchButton.css";
 
 const SearchButton = ({ onClick, onClose }) => (
     <div className="search-button">
-        <i class="fas fa-times-circle fa-2x" onClick={onClose}></i>
+        <i class="far fa-times-circle fa-2x" onClick={onClose}></i>
         <i class="fas fa-chevron-circle-right fa-2x" style={{color:  "blue"}} onClick={onClick}></i>
     </div>
 );
