c16292c4927805d4eb22cf9ea7a630cccc9ce5c0
pritam patil <pritam18121990@gmail.com>
2021-02-18 22:42:49 +0530

    feat(filter-modal): dynamic current year

--- src/constants.js
+++ src/constants.js
@@ -1,35 +1,37 @@
-export const API_KEY = '651925d45022d1ae658063b443c99784';
+export const API_KEY = "651925d45022d1ae658063b443c99784";
 
 export const MOVIE_GENRES = [
-  {"id":28,"name":"Action"},
-  {"id":12,"name":"Adventure"},
-  {"id":16,"name":"Animation"},
-  {"id":35,"name":"Comedy"},
-  {"id":80,"name":"Crime"},
-  {"id":99,"name":"Documentary"},
-  {"id":18,"name":"Drama"},
-  {"id":10751,"name":"Family"},
-  {"id":14,"name":"Fantasy"},
-  {"id":36,"name":"History"},
-  {"id":27,"name":"Horror"},
-  {"id":10402,"name":"Music"},
-  {"id":9648,"name":"Mystery"},
-  {"id":10749,"name":"Romance"},
-  {"id":878,"name":"Science Fiction"},
-  {"id":10770,"name":"TV Movie"},
-  {"id":53,"name":"Thriller"},
-  {"id":10752,"name":"War"},
-  {"id":37,"name":"Western"}
+  { id: 28, name: "Action" },
+  { id: 12, name: "Adventure" },
+  { id: 16, name: "Animation" },
+  { id: 35, name: "Comedy" },
+  { id: 80, name: "Crime" },
+  { id: 99, name: "Documentary" },
+  { id: 18, name: "Drama" },
+  { id: 10751, name: "Family" },
+  { id: 14, name: "Fantasy" },
+  { id: 36, name: "History" },
+  { id: 27, name: "Horror" },
+  { id: 10402, name: "Music" },
+  { id: 9648, name: "Mystery" },
+  { id: 10749, name: "Romance" },
+  { id: 878, name: "Science Fiction" },
+  { id: 10770, name: "TV Movie" },
+  { id: 53, name: "Thriller" },
+  { id: 10752, name: "War" },
+  { id: 37, name: "Western" },
 ];
 
+const currentYear = new Date().getFullYear();
+const MAX_WINDOW = 15;
 export const DEFAULT_FILTERS = {
-  GENRE: 'Comedy',
-  MIN_YEAR: 2000,
-  MAX_YEAR: 2019,
+  GENRE: "Comedy",
+  MIN_YEAR: currentYear - MAX_WINDOW,
+  MAX_YEAR: currentYear,
   MIN_RATING: 7,
   MAX_RATING: 10,
   MIN_TIME_MINS: 60,
-  MAX_TIME_MINS: 120
+  MAX_TIME_MINS: 120,
 };
 
 export const KEY_CODES = {
@@ -39,4 +41,4 @@ export const KEY_CODES = {
 
 export const ITEMS_PER_ROW = 4;
 
-export const TAB_DEFAULT_NAME = 'Now playing';
+export const TAB_DEFAULT_NAME = "Now playing";
