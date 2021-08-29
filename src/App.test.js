cc3e1e67ddb47595e68e898a98a121ff16b57e24
edmondfrancoismillionnaires <edmondfrancoismillionnaires@gmail.com>
2021-08-29 03:29:37 +0000

    add test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,67 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+// @ponicode
+describe("getDerivedStateFromProps", () => {
+    let inst
+
+    beforeEach(() => {
+        inst = new App.default()
+    })
+
+    test("0", () => {
+        let callFunction = () => {
+            inst.getDerivedStateFromProps("reply_click()")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("1", () => {
+        let callFunction = () => {
+            inst.getDerivedStateFromProps(undefined)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
+
+// @ponicode
+describe("componentDidMount", () => {
+    let inst
+
+    beforeEach(() => {
+        inst = new App.default()
+    })
+
+    test("0", () => {
+        let callFunction = () => {
+            inst.componentDidMount()
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
+
+// @ponicode
+describe("componentWillUnmount", () => {
+    let inst
+
+    beforeEach(() => {
+        inst = new App.default()
+    })
+
+    test("0", () => {
+        let callFunction = () => {
+            inst.componentWillUnmount()
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
