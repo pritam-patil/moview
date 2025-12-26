df5453da84443f6cff9cd5d44e0e8cc8de1f6729
Pritam Patil <pritam18@hotmail.com>
2025-12-26 14:32:49 +0530

    lazy loaded header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,28 +1,12 @@
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
-import { Grid, Header } from "semantic-ui-react";
-import { SearchMovie } from "../common";
-import { ROUTES } from "../../constants";
-import "./styles.css";
+import React, { lazy, Suspense } from "react";
+
+const Header = lazy(() => import("./Header"));
 
 const TopbarGrid = (props) => {
   return (
-    <header>
-      <div className="left">
-        <NavLink to={ROUTES.HOME}>
-          <i class="fas fa-film fa-4x"></i>
-        </NavLink>
-        <div>
-          <h2>Moview</h2>
-          <p>Find the right movie!</p>
-        </div>
-      </div>
-      <div className="right">
-        <NavLink to={ROUTES.SETTINGS}>
-          <i class="fas fa-cog fa-2x"></i>
-        </NavLink>
-      </div>
-    </header>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Header />
+    </Suspense>
   );
 };
 
